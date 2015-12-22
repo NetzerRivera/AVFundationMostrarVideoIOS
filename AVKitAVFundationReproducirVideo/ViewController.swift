602e0f4b0a7d6ef83b7f883ce569884117ac1f02
NetzerRivera <netzerrivera@gmail.com>
2015-12-22 00:36:27 -0600

    Versión 1

--- AVKitAVFundationReproducirVideo/ViewController.swift
+++ AVKitAVFundationReproducirVideo/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    private var reproductor =  AVPlayer!()
+    private var controlador =  AVPlayerViewController!()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let videoURL = NSBundle.mainBundle().URLForResource("Atomic", withExtension: "mp4")
+        
+        reproductor = AVPlayer(URL: videoURL!)
+        controlador = AVPlayerViewController()
+        controlador.player = reproductor
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func play() {
+        
+        self.addChildViewController(controlador)
+        controlador.view.frame = CGRectMake(10, 30, 320, 280)
+        self.view.addSubview(controlador.view)
+        reproductor.play()
+    }
 
 }
 
